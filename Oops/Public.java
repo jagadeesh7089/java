2f07526bd3a2a9dc299bf7363505f58ae56e6540
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-07-20 18:24:59 +0530

    ndcd

--- Oops/Public.java
+++ Oops/Public.java
@@ -1,21 +1,36 @@
 
   class Employee{
-    public int  x=100;
-    private int y=433;
-    int z=43;
-    int e=y;
- 
+     public int x;
+     private int y;
+     int z;
+     Employee( int a,int b,int c){
+      this.x=a;
+      this.y=b;
+      this.z=c;
+     }
+
+     private void abc(){
+      System.out.println("this is a Private access method");
+     }
+       void xyz(){
+       System.out.println("this is a Public access method");
+     }
+       void rcb(){
+       System.out.println("But we can  access the private method in the same class of the public method.");
+       abc();
+     }
+     
  }
     public class Public {
     
     public static void main(String[] args) {
        
         // Employee e1=new Public().new Employee();
-        Employee e1=new Employee();
+        Employee e1=new Employee(100,233,445);
         System.out.println(e1.x);
         System.out.println(e1.z);
-        System.out.println(e1.e);
-        System.out.println(e1.y);
+        // System.out.println(e1.y);
+        e1.rcb();
         
     }
     
